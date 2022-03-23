b22ad65322bb5b8f9e5f680df00aa3ea276b8154
Adir Bertão <taober@gmail.com>
2022-03-23 18:26:30 -0300

    acerto de case imagens file

---
+++ app/Models/Imagens.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Imagens extends Model
+{
+    protected $table = 'imagens';
+    protected $primaryKey = 'id';
+}
